4f94a77be35b3b5e7e0f4ba40bdaa8eaec0a2d08
Víctor Raúl Quispe Imata <133170327+Luaroff@users.noreply.github.com>
2023-05-18 22:37:21 -0500

    Mejorando la interfaz del reproductor, linear layouts!

--- app/src/main/java/com/cibertec/yaranime/MainActivity.kt
+++ app/src/main/java/com/cibertec/yaranime/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.cibertec.yaranime
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideExtension
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.annotation.GlideType
 
 class MainActivity : AppCompatActivity() {
 
     private var thisObject = this
+    lateinit var anime:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val timer = object : CountDownTimer(2000, 1000){
+        anime = findViewById(R.id.anime)
+        var url = "https://www.icegif.com/wp-content/uploads/icegif-2013.gif"
+        Uri.parse(url)
+        Glide.with(this).load(url).into(anime)
+        val timer = object : CountDownTimer(5000, 1000){
             override fun onTick(millisUntilFinished: Long) {
 
             }
