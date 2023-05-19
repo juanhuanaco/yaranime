af81dc44d528aac9e230afe4fc8435312dbebb92
Víctor Raúl Quispe Imata <133170327+Luaroff@users.noreply.github.com>
2023-05-18 21:15:56 -0500

    Commit para salvar el reproductor :3

--- app/src/main/java/com/cibertec/yaranime/fragments/MusicFragment.kt
+++ app/src/main/java/com/cibertec/yaranime/fragments/MusicFragment.kt
@@ -1,11 +1,26 @@
 package com.cibertec.yaranime.fragments
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.cibertec.yaranime.R
+import com.cibertec.yaranime.models.Cancion
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.MediaMetadata
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerControlView
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +32,23 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MusicFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MusicFragment : Fragment() {
+class MusicFragment : Fragment(), Player.Listener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    // VARIABLES DE REPRODUCTOR
+    lateinit var player: ExoPlayer
+    lateinit var playerControl: PlayerControlView
+    // VARIABLES DE CONEXION A FIREBASE
+    val database = Firebase.database
+    val myRef = database.getReference("canciones")
+    // VARIABLE TIPO ARRAYLIST PARA RECIBIR NUESTRAS CANCIONES DE FIREBASE
+    lateinit var cancionItems:ArrayList<MediaItem>
+    // VARIABLES PARA CAMBIAR LOS METADATOS SEGUN LA CANCION
+    lateinit var albumImage:ImageView
+    lateinit var title:TextView
+    lateinit var album:TextView
+    lateinit var artist:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +57,61 @@ class MusicFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_music, container, false)
+        albumImage = view.findViewById(R.id.album_portada)
+        title = view.findViewById(R.id.cancion)
+        album = view.findViewById(R.id.album)
+        artist = view.findViewById(R.id.artista)
+        return view
     }
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        playerControl = view.findViewById(R.id.playerControl)
+        cancionItems = ArrayList()
+        getCanciones()
+    }
+    private fun getCanciones() {
+        myRef.get().addOnSuccessListener {
+            for(data in it.children){
+                var auxSong = data.getValue(Cancion::class.java)
+                if(auxSong!=null){
+                    var metadata = MediaMetadata.Builder()
+                        .setTitle(auxSong.title)
+                        .setArtist(auxSong.artist)
+                        .setAlbumTitle(auxSong.album)
+                        .setArtworkUri(Uri.parse(auxSong.img))
+                        .build()
+                    var item:MediaItem = MediaItem.Builder()
+                        .setUri(auxSong.song_url)
+                        .setMediaMetadata(metadata)
+                        .build()
+                    cancionItems.add(item)
+                }
+            }
+            iniciarPlayer()
+        }.addOnFailureListener{
+            Log.e("firebase", "Error getting data", it)
+        }
+    }
+    private fun iniciarPlayer() {
+        player = ExoPlayer.Builder(requireContext()).build()
+        player.repeatMode = Player.REPEAT_MODE_ALL
+        playerControl.player = player
+        player.addMediaItems(cancionItems);
+        player.addListener(this)
+        player.prepare()
+    }
+    override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+        super.onMediaItemTransition(mediaItem, reason)
+        if (mediaItem!=null) {
+            Picasso.get().load(mediaItem.mediaMetadata.artworkUri).into(albumImage);
+            title.text = mediaItem.mediaMetadata.title
+            album.text = mediaItem.mediaMetadata.albumTitle
+            artist.text = mediaItem.mediaMetadata.artist
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
